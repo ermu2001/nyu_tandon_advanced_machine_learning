9f64c48f7f9fe2a554ae6ab28642327824e1e8b4
ermu2001 <782302021@qq.com>
2024-02-25 12:57:52 -0500

    on going HW2

--- HW2/datas.py
+++ HW2/datas.py
@@ -64,6 +64,8 @@ def get_dataset_2d_gaussian(batch_size):
 
 
 def get_dataset_mnist(batch_size):
+    ratio = 0.1 # remaining data as required
+
     transform = transforms.Compose(
         [transforms.ToTensor(),
         transforms.Normalize((0.5,), (0.5,))])
@@ -71,11 +73,14 @@ def get_dataset_mnist(batch_size):
 
     trainset = torchvision.datasets.MNIST(root=DATA_FOLDER, train=True,
                                             download=True, transform=transform)
+    
+    trainset.data = trainset.data[:round(ratio * len(trainset))]
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                             shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.MNIST(root=DATA_FOLDER, train=False,
                                         download=True, transform=transform)
+    testset.data = trainset.data[:round(ratio * len(trainset))]
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                             shuffle=False, num_workers=2)
 
